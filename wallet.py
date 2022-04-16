340908ad3c120b13dfb977695b5a1ef19b70de1a
ProgramerB <Bhavesh2000@outlook.com>
2022-04-16 11:08:11 +0530

    added wallets template

---
+++ wallet.py
@@ -0,0 +1,12 @@
+def twitterKeys():
+	api_key=''
+	api_secret_key=''
+	api_token=''
+	api_secret_token=''
+	return api_key,api_secret_key,api_token,api_secret_token
+
+def redditKeys():
+	client_id=""
+	client_secret=""
+	user_agent=""
+	return client_id,client_secret,user_agent
